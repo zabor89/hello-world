53758712dc23652036790f4dee3ee07a4da383d5
zabor89 <filip.zabor@gmail.com>
2019-03-16 12:30:27 +0100

    Pierwszy commmit

---
+++ liczby.py
@@ -0,0 +1,16 @@
+def przelicz(n=""):
+    w={'jeden':1, 'dwa':2, 'trzy':3, 'cztery':4, 'piec':5, 'szesc':6,
+  'siedem':7, 'osiem':8, 'dziewiec':9, 'dziesiec':10, 'jedenascie':11,
+   'dwanascie':12, 'trzynascie':13, 'czternascie':14, 'pietnascie':15,
+    'szesnascie':16, 'siedemnascie':17, 'osiemnascie':18, 'dziewistnascie':19,
+     'dwadziescia':20, 'trzydziesci':30, 'czterdziesci':40, 'piecdziesiat':50,}
+
+     #l=n.split()
+     #s=0
+    # for x in l:
+        # if x in w:
+            # s+=w[x]
+            # return s
+t=raw_input("Wpisz liczbe od 1 do 59:")
+print ("Wartosc:")
+przelicz(t)
