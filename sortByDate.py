c86a2aa1b1e0b8f6833a99105615423ef3026a74
zabor89 <filip.zabor@gmail.com>
2019-03-26 14:38:29 +0100

    final version

--- sortByDate.py
+++ sortByDate.py
@@ -1,17 +1,16 @@
 import sys, glob, os, time
 
 files=glob.glob("*")
-if sys.argv[1] == "asc":
+if sys.argv[0] == "sortByDate.py":
+    files.sort(key=os.path.getmtime, reverse=False)
+
+elif sys.argv[1] == "asc":
     files.sort(key=os.path.getmtime, reverse=False)
 
 elif sys.argv[1] == "desc":
     files.sort(key=os.path.getmtime, reverse=True)
 
-else :#sys.argv[1] == "":
-    files.sort(key=os.path.getmtime, reverse=False)
-
 for file in files:
     dateString = dateString=time.strftime('%H: %M: %S: %m/ %d/ %Y',
     time.gmtime(os.path.getmtime(file)))
     print (file,dateString)
-#print(sys.argv[1])
